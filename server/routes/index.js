09ae1c4cb2fe693c7f6349cb0ad45b0963e04339
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-06-22 23:52:44 -0700

    created react app, first commit after running command

---
+++ server/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+const client = new pg.Client('postgresql://localhost:5432/strainger_things');
+client.connect();
+client.on('error', err => console.error(err));
+
+
+router.post('/add', function(req, res){
+  client.query(`
+    INSERT INTO gif (strain_id, giphy_id, downsized_large_url) VALUES($1, $2, $3)`, [req.query.strain_id, req.query.giphy_id, req.query.downsized_large_url]
+  )
+    .then(() => res.sendStatus(200))
+    .catch(console.error);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  client.query('SELECT * FROM strain;')
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+module.exports = router;
+
+
+// strain_id | giphy_id | downsized_large_url
