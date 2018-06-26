7b6bacca04996c05394a3523e5942dab41ffdc94
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-06-25 18:02:28 -0700

    rows returning to client but still not allowign me to access them

--- server/client/src/StrainDetail/StrainDetail.js
+++ server/client/src/StrainDetail/StrainDetail.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react'
 class StrainDetail extends Component {
   constructor(props) {
     super(props)
-    this.state= {
-
+    this.state = {
+      selectedStrain: []
     }
   }
 
-  // componentDidMount() {
-  //   fetch('http://localhost:3001/strainDetail')  
-  //   .then(response => response.json())
-  //   .then(response => this.setState({ strains: response.data }))
-  //   // .then(({ data }) => {
-  //   //   console.log(data)
-  //   // })
-  // }
+  componentDidMount() {
+    fetch(`http://localhost:3001/strainDetail/${this.props.selectedStrainId}`)
+    .then(response => response.json())
+    .then(response => this.setState({ selectedStrain: response.data }))
+    // .then(({ data }) => {
+    //   console.log(data)
+    // })
+  }
 
   render() {
     return(
-      <div>Strain Detail {this.props.selectedStrainId}</div>
+      <div>Strain Detail {this.state.selectedStrain[0]}</div>
     )
   }
 }
